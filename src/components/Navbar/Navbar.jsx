b4a77c055d6df42ac4c8d7da9d4ec8ef87a4c3e5
John Hash <johnhash@frozen.com>
2025-02-25 21:20:26 +0100

    feat(Navbar): add Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './navbar.css';
+const Navbar = () => {
+    return (
+        <div className="navbar-container">
+            <div>HOME</div>
+            <div>PUZZLES</div>
+            <div>ADD</div>
+            <div>MESSAGES</div>
+            <div>PROFILE</div>
+        </div>
+    )
+}
+
+export default Navbar;
