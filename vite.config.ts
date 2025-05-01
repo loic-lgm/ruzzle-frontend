9a5471fd6837239cf3dc37861979ff6f0fc1aeef
John Hash <johnhash@frozen.com>
2025-05-01 17:38:00 +0200

    chore(TypeScript): migrate to typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,9 +13,14 @@ export default defineConfig({
     react(),
     eslintPlugin({
       cache: false,
-      include: ['./src//*.js', './src//*.jsx'],
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
       failOnError: false,
       emitWarning: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 });
