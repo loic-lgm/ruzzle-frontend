76ca6ddbe8cfbade0620906da4240f00c4d67227
JohnHash <john@hash.com>
2025-08-09 21:57:00 +0200

    feat(chore): scroll to top after navigate()

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 import Home from './pages/Home';
 import { useFetchUser } from '@/hooks/useFetchUser';
 import Puzzles from '@/pages/Puzzles';
@@ -8,6 +8,17 @@ import Publish from '@/pages/Publish';
 import { Toaster } from 'sonner';
 import Profile from '@/pages/Profile';
 import PublicProfilePage from '@/pages/PublicProfile';
+import React, { useLayoutEffect } from 'react';
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location.pathname]);
+
+  return children;
+};
 
 const App = () => {
   const { isLoading } = useFetchUser();
@@ -22,17 +33,19 @@ const App = () => {
     <>
       <Toaster position="top-right" richColors />
       <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/puzzles" element={<Puzzles />} />
-            <Route path="/ajouter-un-puzzle" element={<Publish />} />
-            <Route path="/mon-espace" element={<Profile />} />
-            <Route path="/profil/:username" element={<PublicProfilePage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            {/* <Route path="*" element={<NotFound />} /> */}
-          </Route>
-        </Routes>
+        <Wrapper>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/puzzles" element={<Puzzles />} />
+              <Route path="/ajouter-un-puzzle" element={<Publish />} />
+              <Route path="/mon-espace" element={<Profile />} />
+              <Route path="/profil/:username" element={<PublicProfilePage />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              {/* <Route path="*" element={<NotFound />} /> */}
+            </Route>
+          </Routes>
+        </Wrapper>
       </BrowserRouter>
     </>
   );
