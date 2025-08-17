1204ccfbfde1ff52720d79ddaf5daf8dc9b0e538
JohnHash <john@hash.com>
2025-08-17 18:30:04 +0200

    feat(SearchBar): add icon to delete search terms

--- src/components/Navbar/components/SearchBar/SearchBar.tsx
+++ src/components/Navbar/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Search, User } from 'lucide-react';
+import { Search, User, X } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import React, { useState } from 'react';
 import { useSearch } from '@/hooks/useSearch';
@@ -27,17 +27,28 @@ const SearchBar = () => {
     setIsOpen(false);
   };
 
+  const handleDelete = () => {
+    setSearchTerm('');
+    setIsOpen(false);
+  }
+
   return (
     <div className="relative flex items-center w-1/2">
-      <Search className="absolute left-[10px] z-[99] h-4 w-4 text-muted-foreground transition-colors peer-focus:text-green-500" />
+      <Search className="absolute left-[10px] z-[99] h-4 w-4 text-muted-foreground transition-colors" />
       <Input
         id="searchbar"
         type="text"
         placeholder="Rechercher un membre"
-        className="peer pl-10 pr-4 rounded-full border-2 border-border/50 bg-background/80 backdrop-blur-sm focus:border-primary/50 focus:bg-background transition-all"
+        className="pl-10 pr-4 rounded-full border-2 border-border/50 bg-background/80 backdrop-blur-sm focus:border-primary/50 focus:bg-background transition-all"
         value={searchTerm}
         onChange={handleValueChange}
       />
+      {searchTerm && (
+        <X
+          className="absolute right-[10px] z-[99] h-4 w-4 text-muted-foreground transition-colors cursor-pointer"
+          onClick={handleDelete}
+        />
+      )}
       {isOpen && (
         <Card className="absolute top-full left-0 right-0 mt-2 py-2 bg-background/95 backdrop-blur-md border shadow-lg z-50 max-h-80 overflow-y-auto">
           {searchResults.length > 0
