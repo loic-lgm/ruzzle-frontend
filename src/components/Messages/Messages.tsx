deef7e36fabca389d90c236a1e2bbbaf1364538f
JohnHash <john@hash.com>
2025-08-28 16:41:19 +0200

    feat(Message): display date before time in message

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -105,7 +105,28 @@ const Messages = ({
 
   const formatTime = (timestamp: string) => {
     const date = new Date(timestamp);
-    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const now = new Date();
+
+    const isToday = date.toDateString() === now.toDateString();
+    const yesterday = new Date();
+    yesterday.setDate(now.getDate() - 1);
+    const isYesterday = date.toDateString() === yesterday.toDateString();
+
+    const time = date.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+
+    if (isToday) {
+      return time;
+    } else if (isYesterday) {
+      return `Hier, ${time}`;
+    } else {
+      return `${date.toLocaleDateString([], {
+        day: '2-digit',
+        month: 'short',
+      })}, ${time}`;
+    }
   };
 
   const formatDate = (created: string) => {
