e2f065c4be44c5e7084dee28c91a3cd666a60e93
JohnHash <john@hash.com>
2025-09-24 19:15:11 +0200

    feat(Analytics): add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from '@/App';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ if (rootElement) {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Analytics />
       </QueryClientProvider>
     </StrictMode>
   );
